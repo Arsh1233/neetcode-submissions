6a242adc0ed0c6a43a8f23901be70f2ec3f2585b
Arsh Chakraborty <139649276+Arsh1233@users.noreply.github.com>
2026-07-03 12:12:00 +0530

    Add: sqrtx - submission-1

---
+++ Data Structures & Algorithms/sqrtx/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        low=0
+        high=x
+        res=0
+        while low<=high:
+            mid=(low +high )//2
+            if (mid*mid)>x:
+                high=mid-1
+            elif (mid*mid)<x :
+                res=mid
+                low=mid+1
+            else:
+                return mid  
+        return int(res)
